1a582d609386743ca18e9be6a11b01f9f0ceb0b7
Amir Awadalla <amir@Amirs-MacBook-Air-140.local>
2026-07-02 23:22:21 -0700

    Added script which saves CL_VV txt

---
+++ scripts/SphericalPlot.py
@@ -0,0 +1,22 @@
+import numpy as np
+from pop3_cmb.config import PARAMS, CONSTANTS, COSMOLOGY
+from pop3_cmb.cosmology import CAMBRunner
+from pop3_cmb.faraday import FaradayModel
+from pop3_cmb.spectrum import VVSpectrum
+
+def main():
+    camb_run = CAMBRunner(COSMOLOGY, PARAMS['k_min'], PARAMS['k_max'], PARAMS['n_k_points'], PARAMS['z_min'], PARAMS['z_max'])
+    z_grid, k_grid, Plin = camb_run.get_linear_matter_power()
+    r_grid = camb_run.get_comoving_distance(z_grid)
+    ls_ee, Cl_EE = camb_run.get_primordial_Cl_EE(lmax=3000)
+
+    fm = FaradayModel(camb_run, PARAMS, CONSTANTS, k_grid, Plin)
+    P_alpha = fm.compute_P_alpha()
+
+    convolution = VVSpectrum(2, PARAMS['ell_max'], P_alpha, z_grid, r_grid, k_grid, Cl_EE)
+    ls, Cl_VV = convolution.compute_Cl_VV()
+    np.savetxt('./Cl_VV_Spherical.txt', np.column_stack((ls, Cl_VV)), header='ell Cl_VV')
+
+if __name__ == "__main__":
+    main()
+
